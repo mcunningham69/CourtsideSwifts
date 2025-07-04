450020cf8402633577cc5a95ec4724e2891efa3a
Mike Cunningham <mcunningham69@icloud.com>
2025-07-04 13:26:14 +0800

    Initial Commit

---
+++ CourtsideSwiftsTests/CourtsideSwiftsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourtsideSwiftsTests.swift
+//  CourtsideSwiftsTests
+//
+//  Created by Mike Cunningham on 4/7/2025.
+//
+
+import Testing
+@testable import CourtsideSwifts
+
+struct CourtsideSwiftsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
